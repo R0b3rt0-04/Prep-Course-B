ceb336bcd853bff11497ce2f26f603570ce8f172
unknown <robertosaulh@gmail.com>
2022-11-02 14:31:26 -0600

    05-JS-V Corrección

--- 05-JS-V/homework/homework.js
+++ 05-JS-V/homework/homework.js
@@ -1,57 +1,67 @@
-// Do not change any of the function names
+// No cambies los nombres de las funciones.
 
-function mayuscula(nombre) {
-  //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
-  //ej: Recibe "mario" ----> Devuelve "Mario"
-  //Tu código:
+function crearUsuario() {
+  // Crea una Clase de ES6 o una función constructor llamada "Usuario"
+  // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
+  // En el `contructor`, define el usuario, el nombre, el email y la contraseña
+  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // {{nombre}} debe ser el nombre definido en cada instancia
+  // Devuelve la clase
+  // Tu código:
 }
 
-function invocarCallback(cb) {
-  // Invoca al callback `cb`
-  //Tu código:
+function agregarMetodoPrototype(Constructor) {
+  // Agrega un método al Constructor del `prototype`
+  // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
+  // Tu código:
 }
 
-function operacionMatematica(n1, n2, cb) {
-  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
-  //Devolver el callback pasándole como argumentos los números recibidos.
-  //Tu código:
+function agregarStringInvertida() {
+  // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
+  // El método debe llamarse "reverse"
+  // Ej: 'menem'.reverse() => menem
+  // 'toni'.reverse() => 'inot'
+  // Pista: Necesitarás usar "this" dentro de "reverse"
 }
 
-function sumarArray(numeros, cb) {
-  // Suma todos los números enteros (int/integers) de un array ("numeros")
-  // Pasa el resultado a `cb`
-  // No es necesario devolver nada
-  //Tu código:
+// ---------------------------------------------------------------------------//
+  //Crea el constructor de la clase "Persona"
+  //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
+  //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
+  //Ej: { 
+    //   Nombre: 'Juan',
+    //   Apellido: 'Perez',
+    //   Edad: 22,
+    //   Domicilio: 'Saavedra 123'
+    //  }
+
+  class Persona {
+    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+      // Crea el constructor:
+
+    }
 }
 
-function forEach(array, cb) {
-  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
-  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
-  //Tu código:
+function crearInstanciaPersona(nombre, apellido, edad, dir) {
+  //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
+  //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
+  //Devolver la nueva persona creada
 }
-
-function map(array, cb) {
-  // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
-  // El nuevo array debe tener la misma longitud que el array del argumento
-  //Tu código:
+  
+function agregarMetodo() {
+  //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
+  //Ej: "Juan, 22 años"
 }
-
-function filter(array) {
-  //Filtrar todos los elementos del array que comiencen con la letra "a".
-  //Devolver un nuevo array con los elementos que cumplen la condición
-  //Tu código:
-}
+  
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
-  mayuscula,
-  invocarCallback,
-  operacionMatematica,
-  sumarArray,
-  forEach,
-  map,
-  filter
+  crearUsuario,
+  agregarMetodoPrototype,
+  agregarStringInvertida,
+  crearInstanciaPersona,
+  agregarMetodo, 
+  Persona
 };
